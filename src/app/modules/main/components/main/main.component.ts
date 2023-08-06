12f6cd219b733deb0510c6492657d1676b7cd4c4
krapal07 <krapal.chahar@ril.com+1-at-460015929520>
2023-08-07 02:02:20 +0530

    blog ui change

--- src/app/modules/main/components/main/main.component.ts
+++ src/app/modules/main/components/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,8 +6,10 @@ import { Router } from '@angular/router';
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
   constructor(private router: Router) {}
+
+  ngOnInit(): void {}
 
   createWish() {
     this.router.navigate(['/create-wishes']);
