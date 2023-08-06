095b62d4e7882a695ebf6e34c6f278c0af88bdb6
krapal07 <krapal.chahar@ril.com+1-at-460015929520>
2023-08-06 20:38:45 +0530

    about and contact us content added

--- src/app/modules/about-us/about-us.module.ts
+++ src/app/modules/about-us/about-us.module.ts
@@ -4,14 +4,8 @@ import { CommonModule } from '@angular/common';
 import { AboutUsRoutingModule } from './about-us-routing.module';
 import { AboutUsComponent } from './about-us.component';
 
-
 @NgModule({
-  declarations: [
-    AboutUsComponent
-  ],
-  imports: [
-    CommonModule,
-    AboutUsRoutingModule
-  ]
+  declarations: [AboutUsComponent],
+  imports: [CommonModule, AboutUsRoutingModule],
 })
-export class AboutUsModule { }
+export class AboutUsModule {}
--- src/app/modules/contact-us/contact-us.component.ts
+++ src/app/modules/contact-us/contact-us.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.scss']
+  styleUrls: ['./contact-us.component.scss'],
 })
 export class ContactUsComponent {
+  showToaster = false;
 
+  submit() {
+    this.showToaster = true;
+
+    setTimeout(() => {
+      this.showToaster = false;
+    }, 3000);
+  }
 }
