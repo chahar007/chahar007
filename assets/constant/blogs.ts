a37e701615a0d9962ea35a4f79015d12580e4d76
krapal07 <krapal.chahar@ril.com+1-at-460015929520>
2023-08-07 01:44:42 +0530

    Auto-generated commit [ci skip]

---
+++ assets/constant/blogs.ts
@@ -0,0 +1,47 @@
+export const BLOGS_CONTENT = [
+  {
+    id: 'indian-independence-day',
+    data: {
+      title: 'Indian Independence: A Triumph of Freedom and Unity',
+      desc: `Indian Independence is a defining moment in the history of India, representing the country's victory over colonial oppression and its journey towards becoming a sovereign nation. On August 15, 1947, after years of struggle, India finally gained independence from British rule. This momentous occasion marked the beginning of a new era for the nation, setting the stage for progress, democracy, and unity.`,
+      keyPoints: [
+        {
+          title: 'Freedom Struggle',
+          desc: 'The Indian independence movement was a courageous battle led by visionaries like Mahatma Gandhi, Jawaharlal Nehru, and Subhas Chandra Bose. Their commitment to non-violence and civil disobedience ignited the flames of freedom across the nation.',
+        },
+        {
+          title: 'Salt March (Dandi March)',
+          desc: 'A symbol of resistance, the Salt March led by Mahatma Gandhi was a non-violent protest against the British-imposed salt tax. It galvanized millions and ignited the spirit of civil disobedience.',
+        },
+        {
+          title: 'Quit India Movement',
+          desc: 'In 1942, the Quit India Movement was launched, demanding an immediate end to British rule. This mass movement exemplified the power of united resistance and paved the way for freedom.',
+        },
+        {
+          tittle: '',
+          desc: `The Indian independence struggle was marked by the unity of people from diverse backgrounds, languages, and cultures. This collective consciousness fueled the movement and eventually led to the downfall of British imperialism. The power of non-violence demonstrated by Mahatma Gandhi played a crucial role in showcasing India's unwavering determination.`,
+        },
+        {
+          title: '',
+          desc: `The oppressive policies and exploitation under the British Raj had a profound impact on the Indian psyche. From the Jallianwala Bagh massacre to the Bengal Famine, the atrocities fueled the nation's quest for self-determination and independence. The courage and resilience of countless freedom fighters who faced imprisonment and persecution inspired the masses to stand together for a common cause.`,
+        },
+        {
+          title: 'Constitution of India',
+          desc: 'The drafting of the Indian Constitution by Dr. B.R. Ambedkar laid the foundation for a progressive, secular, and democratic nation.',
+        },
+        {
+          title: 'Challenges of Partition',
+          desc: 'The partition of India and the subsequent creation of Pakistan led to the tragic displacement of millions and communal violence. It was a challenging chapter in Indias history.',
+        },
+        {
+          title: 'Celebrating Diversity',
+          desc: `India's independence celebrates its rich cultural heritage, embracing multiple religions, languages, and traditions. Unity in diversity remains the essence of the Indian spirit.`,
+        },
+        {
+          title: 'Summary',
+          desc: 'Indian Independence Day is a time to reflect on the sacrifices of the past and the vision for the future. As we commemorate this historic occasion, let us cherish our freedom, honor the struggles of our ancestors, and strive to build a united, inclusive, and prosperous India. Together, we can uphold the legacy of our freedom fighters and contribute to the growth and betterment of our great nation.',
+        },
+      ],
+    },
+  },
+];
